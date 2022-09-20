5930ed703f240919005cc32092b4ebedc93c7dbf
rookedsysc <rookedsysc36@gmail.com>
2022-09-21 04:19:37 +0900

    주석

--- flutterProject/hello_world/lib/main.dart
+++ flutterProject/hello_world/lib/main.dart
@@ -5,8 +5,11 @@ void main() {
     // Materail App은 반드시 들어가야 함. 국룰임.
     MaterialApp(
       home: Scaffold(
+        // background color
         backgroundColor: Colors.black,
+        // 글자 중앙에 배치
         body: Center(
+          // 글자 색상, 크기
           child: Text(
             'Hello World',
             style: TextStyle(
