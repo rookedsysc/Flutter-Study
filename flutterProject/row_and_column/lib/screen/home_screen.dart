1ebfdc2d7f85156b3d4f88b474aa6626bfe97dbf
rookedsysc <rookedsysc36@gmail.com>
2022-09-22 07:35:40 +0900

    Column mainAixisAlignment

--- flutterProject/row_and_column/lib/screen/home_screen.dart
+++ flutterProject/row_and_column/lib/screen/home_screen.dart
@@ -12,6 +12,15 @@ class HomeScreen extends StatelessWidget {
         child: Container(
           color: Colors.black,
           child: Column(
+            /* MainAxisAlignment - 주축 정렬.
+            start - 시작에 정렬.
+            end - 끝에 정렬.
+            center - 가운데 정렬.
+            spaceBetween - 시작과 끝에 배치되고 위젯과 위젯의 사이가 동일하기 배치됨.
+            spaceEvenly - 위젯을 같은 간격으로 배치하지만 끝과 끝에도 위젯이 아닌 빈 간격으로 시작함.
+            spaceArround - spaceEvenly에서 끝과 끝의 간격을 반만 줌.
+             */
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 color: Colors.red,
