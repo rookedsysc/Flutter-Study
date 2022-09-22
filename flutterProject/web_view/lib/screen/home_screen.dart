37bb845d5b2323a5f0d3c4d053e36b77c54aceec
rookedsysc <rookedsysc36@gmail.com>
2022-09-22 14:16:51 +0900

    Complete Web View

--- flutterProject/web_view/lib/screen/home_screen.dart
+++ flutterProject/web_view/lib/screen/home_screen.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  WebViewController? controller;
+  final homeUrl = 'https://rookedsysc.github.io';
+
+  // const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +16,27 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: Text('Rookedsysc\'s Blog'),
         centerTitle: true,
+
+        // App Bar에 아이콘 만들어줌
+        actions: [IconButton(onPressed: () { // Icon onPressed 동작시 실행할 함수
+          if (controller == null) {
+            return;
+          } else {
+            // 절대로 null 값이 들어갈 수 없다는 것을 알려줌.
+            controller!.loadUrl(homeUrl);
+          }
+        }, icon: Icon(
+          Icons.home,
+        ))],
       ),
       body: SafeArea(
         child: WebView(
-          initialUrl: 'https://rookedsysc.github.io',
-            // 기본 값은 disable로 javascript를 못 읽어오게 되어있음.
+          // WebView에서 할 동작을 Controller에 정의해주고 실행시켜줌.
+          onWebViewCreated: (WebViewController controller) {
+            this.controller = controller;
+          },
+          initialUrl: homeUrl,
+          // 기본 값은 disable로 javascript를 못 읽어오게 되어있음.
           javascriptMode: JavascriptMode.unrestricted,
         ),
       ),
