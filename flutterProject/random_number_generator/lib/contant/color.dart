78535d9e419a53a8434a57e893bfc6299678aad5
rookedsysc <rookedsysc36@gmail.com>
2022-09-26 02:02:59 +0900

    map()을 이용해서 HomeScreen에 그림 불러오기

--- flutterProject/random_number_generator/lib/contant/color.dart
+++ flutterProject/random_number_generator/lib/contant/color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // 주로 사용하는 색상 모음
-const Color PRIMARY_COLOR = Color(0xFF2D2D33); // 배경 색상
-const Color RED_COLOR = Color(0xFFEA4955);
-const Color BLUE_COLOR = Color(0xFF549FBF);
+const Color primaryColor = Color(0xFF2D2D33); // 배경 색상
+const Color redColor = Color(0xFFEA4955);
+const Color blueColor = Color(0xFF549FBF);
 
--- flutterProject/random_number_generator/lib/screen/home_screen.dart
+++ flutterProject/random_number_generator/lib/screen/home_screen.dart
@@ -12,9 +12,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: PRIMARY_COLOR,
+      backgroundColor: primaryColor,
       body: SafeArea(
-        child: Padding( // Column을 padding으로 감싸줘서 Column과 화면사이의 간격을 둠.
+        child: Padding(
+          // Column을 padding으로 감싸줘서 Column과 화면사이의 간격을 둠.
           /*
           All: 사방에 같은 간격을 줌,
           formLTRB(left, top, right, bottom)
@@ -22,7 +23,8 @@ class _HomeScreenState extends State<HomeScreen> {
           sysmmetric(horizontal, vertical): 가로 / 세로
            */
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
               Text(
                 '랜덤 숫자 생성기',
@@ -37,26 +39,42 @@ class _HomeScreenState extends State<HomeScreen> {
                 onPressed: () {},
                 icon: Icon(
                   Icons.settings,
-                  color: RED_COLOR,
+                  color: redColor,
                 ),
               ),
             ]),
             Expanded(
-                child: Column(
-              // 가운데 부분의 숫자가 남은 공간 전부를 차지함.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('123'),
-                Text('456'),
-                Text('789'),
-              ],
-            )),
+              child: Column(
+                  // 가운데 부분의 숫자가 남은 공간 전부를 차지함.
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    123,
+                    456,
+                    789,
+                  ]
+                      .map(
+                        (x) => Row(
+                          children: x
+                              .toString()
+                              .split('')
+                              .map(
+                                (e) => Image.asset(
+                                  'asset/img/$e.png',
+                                  height: 70.0,
+                                  width: 50.0,
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      )
+                      .toList()),
+            ),
             SizedBox(
               // SizedBox는 너비와 높이만 정할 수 있음. 간단한 대신 정~~~말 조금 더 빠름.
               width: double.infinity, // 무한대 값을 넣어줘도 삐져나가지 않음
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  primary: RED_COLOR, // 활성화 됐을 때의 색상
+                  primary: redColor, // 활성화 됐을 때의 색상
                 ),
                 onPressed: () {},
                 child: Text('생성하기!'),
