9b600c13a7d41a26c4a739a161f0bc41d99ea99b
rookedsysc <rookedsysc36@gmail.com>
2022-09-25 23:43:34 +0900

    Layout 정렬, Style 주기 + Padding Widget 사용

---
+++ flutterProject/random_number_generator/lib/contant/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// 주로 사용하는 색상 모음
+const Color PRIMARY_COLOR = Color(0xFF2D2D33); // 배경 색상
+const Color RED_COLOR = Color(0xFFEA4955);
+const Color BLUE_COLOR = Color(0xFF549FBF);
+
--- flutterProject/random_number_generator/lib/screen/home_screen.dart
+++ flutterProject/random_number_generator/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/contant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,37 +12,58 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween
-
-              , children: [
-            Text('랜덤 숫자 생성기'),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.settings
+        child: Padding( // Column을 padding으로 감싸줘서 Column과 화면사이의 간격을 둠.
+          /*
+          All: 사방에 같은 간격을 줌,
+          formLTRB(left, top, right, bottom)
+          only(named parameter)
+          sysmmetric(horizontal, vertical): 가로 / 세로
+           */
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(
+                '랜덤 숫자 생성기',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
+              IconButton(
+                // 설정 버튼
+                onPressed: () {},
+                icon: Icon(
+                  Icons.settings,
+                  color: RED_COLOR,
+                ),
+              ),
+            ]),
+            Expanded(
+                child: Column(
+              // 가운데 부분의 숫자가 남은 공간 전부를 차지함.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('123'),
+                Text('456'),
+                Text('789'),
+              ],
+            )),
+            SizedBox(
+              // SizedBox는 너비와 높이만 정할 수 있음. 간단한 대신 정~~~말 조금 더 빠름.
+              width: double.infinity, // 무한대 값을 넣어줘도 삐져나가지 않음
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: RED_COLOR, // 활성화 됐을 때의 색상
+                ),
+                onPressed: () {},
+                child: Text('생성하기!'),
+              ),
+            )
           ]),
-          Expanded(child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('123'),
-              Text('456'),
-              Text('789'),
-            ],
-          )),
-          SizedBox( // SizedBox는 너비와 높이만 정할 수 있음. 간단한 대신 정~~~말 조금 더 빠름.
-            width: double.infinity, // 무한대 값을 넣어줘도 삐져나가지 않음
-            child: ElevatedButton(onPressed: (){}, child: Text('생성하기!'),
-            ),
-          )
-
-        ]),
+        ),
       ),
     );
   }
