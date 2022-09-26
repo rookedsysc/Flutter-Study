e1bfdc71163de7c47e3a1f0ed5a4c63dce631657
rookedsysc <rookedsysc36@gmail.com>
2022-09-26 20:59:26 +0900

    최대 숫자 Setting Screen에서 넘겨받아서 설정해줌.

--- flutterProject/random_number_generator/lib/screen/home_screen.dart
+++ flutterProject/random_number_generator/lib/screen/home_screen.dart
@@ -12,6 +12,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<int> randomNumbers = [123, 456, 789];
+  int maxNumber = 1000;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            _Header(),
+            _Header(onPressed: onPopSetting),
             _Body(randomNumbers: randomNumbers),
             _Footer(onPressed: onRandomNumberGenerate),
           ]),
@@ -38,6 +39,24 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  void onPopSetting() async { // await 사용하기 위해서 async 상속.
+    // 화면을 이동하기 위해서 사용하는 Class.
+    // 미래의 특정한 시점(pop)될 때 받을 값이므로 await 사용.
+    // push에다가 돌려받을 값의 generic을 지정해줄 수도 있음.
+    final result = await Navigator.of(context).push<int>( // 항상 navigation에서 return 해주는 값은 null 값이 있을 수도 있다는 것은 가정.
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return SettingScreen(); // context에 Screen을 반환해줌.
+        },
+      ),
+    );
+    // 뒤로 가기 버튼을 누르게 되면 result에 null값이 들어가게 됨.
+    if (result != null) {
+      setState(() { // build해서 재실행 해줘야 하는 값들은 항상 setState를 통해서 넘겨줘야 함.
+        maxNumber = result;
+      });
+  }}
+
   // setState를 Stateless Widget에서 사용할 수 없기 때문에 여기로 옮겨준거임.
   void onRandomNumberGenerate() {
     // 버튼을 누를 때마다 1000이하의 숫자 3개를 리스트로 생성해줌.
@@ -45,7 +64,7 @@ class _HomeScreenState extends State<HomeScreen> {
     final Set<int> newNumbers = {}; // 리스트로 값을 받게 되면 중복 값이 들어감.
     // for문을 사용하게 되면 중복값이 들어갔을 때 최종적으로 2개만 들어가 있게 됨. (Set 특성상)
     while (newNumbers.length < 3) {
-      final number = rand.nextInt(1000); // nextInt(1000) > 최대값.
+      final number = rand.nextInt(maxNumber); // nextInt(1000) > 최대값.
       newNumbers.add(number);
     }
 
@@ -58,7 +77,8 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class _Header extends StatelessWidget {
-  const _Header({Key? key}) : super(key: key);
+  final VoidCallback onPressed; // onPressed Button을 main class에서 직접 받기
+  const _Header({required this.onPressed, Key? key}) : super(key: key); // structor 설정
 
   @override
   Widget build(BuildContext context) {
@@ -73,16 +93,7 @@ class _Header extends StatelessWidget {
       ),
       IconButton(
         // 설정 버튼
-        onPressed: () {
-          // 화면을 이동하기 위해서 사용하는 Class
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (BuildContext context) {
-                return SettingScreen(); // context에 Screen을 반환해줌.
-              },
-            ),
-          );
-        },
+        onPressed: onPressed,
         icon: Icon(
           Icons.settings,
           color: redColor,
--- flutterProject/random_number_generator/lib/screen/setting_screen.dart
+++ flutterProject/random_number_generator/lib/screen/setting_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:random_number_generator/contant/color.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -9,12 +9,53 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  double maxNumber = 1000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: (
-        Text('Hello Wolrd')
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                    children: maxNumber
+                        .toString()
+                        .split('')
+                        .map(
+                          (e) => Image.asset(
+                            'asset/img/$e.png',
+                            width: 50.0, height: 70.0,
+                          ),
+                        )
+                        .toList(),
+                ),
+              ),
+              Slider(value: maxNumber, min: 1000, max: 100000, onChanged: (double val) {
+                setState(() {
+                  // val의 값이 바뀔 때마다 maxNumber를 변경해줌.
+                  // 이렇게 해주지 않으면 rebuild 과정을 거치지 않아서 Slider의 버튼이 움직이지 않음.
+                  maxNumber = val;
+                });
+
+                },// ValueChange<double> 함수
+              ),
+              ElevatedButton(onPressed: (){// 저장 버튼을 누르면 홈으로 돌아감.
+                Navigator.of(context).pop(
+                  maxNumber.toInt() // 홈스크린의 push한 위치로 데이터 넘겨줌.
+                );
+
+              },
+                  style: ElevatedButton.styleFrom(
+                    primary: redColor
+                  ),
+                  child: Text('저장!',))
+            ],
+          ),
         ),
       ),
     );
