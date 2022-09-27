44a9d2199d72155a931397fa49dd1611a4fb041e
rookedsysc <rookedsysc36@gmail.com>
2022-09-28 04:46:58 +0900

    canPop, maybePop, WillPopScop Widget

--- Theory/navigation/lib/screen/route_main_screen.dart
+++ Theory/navigation/lib/screen/route_main_screen.dart
@@ -4,6 +4,7 @@ import 'package:navigation/screen/route_two_screen.dart';
 
 class RouteMainScreen extends StatelessWidget {
   final int? number;
+
   const RouteMainScreen({this.number, Key? key}) : super(key: key);
 
   @override
@@ -11,28 +12,40 @@ class RouteMainScreen extends StatelessWidget {
     return MainLayout(
       title: 'Main Route Screen',
       children: [
-        Text('arguments: ${number.toString()}',
-        // Text Field안에서 가운데로 정렬됨.
-        textAlign: TextAlign.center,),
+        Text(
+          'arguments: ${number.toString()}',
+          // Text Field안에서 가운데로 정렬됨.
+          textAlign: TextAlign.center,
+        ),
         ElevatedButton(
             onPressed: () {
-              Navigator.of(context).pop(3030303);
+              print(Navigator.of(context)
+                  .canPop()); // 아무런 pop 동작도 하지 않고 return만 주는 역할.
             },
-            child: Text('Pop'),
-    ),
-        ElevatedButton(onPressed: (){
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (BuildContext context) => RouteTwoScreen(),
-            // push의 Setting을 통해서 다른 스크린에 값을 전달 해줄 수 있음.
-            settings: RouteSettings(
-              arguments: 789
-            )),
-          );
-        }, child:
-          Text('Push'),
+            child: Text('Can Pop')),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).maybePop();
+            },
+            child: Text('Maybe Pop')),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop(3030303);
+          },
+          child: Text('Pop'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                  builder: (BuildContext context) => RouteTwoScreen(),
+                  // push의 Setting을 통해서 다른 스크린에 값을 전달 해줄 수 있음.
+                  settings: RouteSettings(arguments: 789)),
+            );
+          },
+          child: Text('Push'),
         ),
       ],
     );
   }
 }
-
