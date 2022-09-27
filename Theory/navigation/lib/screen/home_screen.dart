ecf230910a87e2ebbd2904e7259d78ff0b8095ec
rookedsysc <rookedsysc36@gmail.com>
2022-09-28 00:27:37 +0900

    코드 정리

--- Theory/navigation/lib/screen/home_screen.dart
+++ Theory/navigation/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 import 'package:navigation/screen/route_main_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,22 +12,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [ElevatedButton(onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (BuildContext context) => RouteMainScreen(),)
-            );
-          }, child: Text('Push'))],
-        ),
-      ),
-    );
+    return MainLayout(title: 'HomeScreen', children: [
+      ElevatedButton(onPressed: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (BuildContext cotnext) => RouteMainScreen()),
+        );
+      }, child: Text('Push'))
+    ]);
   }
 }
--- Theory/navigation/lib/screen/route_main_screen.dart
+++ Theory/navigation/lib/screen/route_main_screen.dart
@@ -1,28 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 
 class RouteMainScreen extends StatelessWidget {
   const RouteMainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Route'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('Pop'))
-          ],
-        ),
-      ),
-    );
+    return MainLayout(title: 'Main Route Screen', children: [
+      ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Pop'))
+    ],);
   }
 }
