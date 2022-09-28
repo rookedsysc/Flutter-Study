c7a0f997c86b4306fff738b69b09d23f7e9c78c1
rookedsysc <rookedsysc36@gmail.com>
2022-09-29 04:30:19 +0900

    위치 권한 설정 확인하는 로직 구현

--- Project/going_to_work_again/lib/screen/home_screen.dart
+++ Project/going_to_work_again/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -27,10 +28,39 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Column(
           children: [
             _CustomGoogleMap(initialPosition: initialPosition,),
-            _ChoolCheckButton()
+            _ChoolCheckButton(),
           ],
         ));
   }
+
+  Future<String> checkPermission() async {
+    // 권한 요청과 관련된 모든 요청은 async로 작업함.
+    final isLocationEnabled = await Geolocator
+        .isLocationServiceEnabled(); // 위치 서비스 활성화 유/무
+
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    LocationPermission checkedPermission = await Geolocator
+        .checkPermission(); // 위치 서비스 권한 확인
+
+    if (checkedPermission ==
+        LocationPermission.denied) { // Permission 요청을 할 수 있는 상태
+      checkedPermission = await Geolocator.requestPermission(); // 권한 요청
+
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱을 위치 권한을 세팅에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가되었습니다.';
+  }
+}
   AppBar renderAppBar() {
     return AppBar(
       backgroundColor: Colors.white,
