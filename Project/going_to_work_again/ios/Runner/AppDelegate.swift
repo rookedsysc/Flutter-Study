0a237b307305d292dfdc44caef6b81837c007060
rookedsysc <rookedsysc36@gmail.com>
2022-09-29 03:42:50 +0900

    Geometry, Google Maps Setting

--- Project/going_to_work_again/ios/Runner/AppDelegate.swift
+++ Project/going_to_work_again/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyB3ocH1nB3VHLM0XxXw5B6lu7bSpkFq1kI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
