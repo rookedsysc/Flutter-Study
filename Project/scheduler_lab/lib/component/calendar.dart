5e97d0658b671b5118f8fe558ff14b7cc579d988
rookedsysc <rookedsysc36@gmail.com>
2022-10-03 23:19:26 +0900

    Talbe Calendar Design 완료 +Design 사용 방법

--- Project/scheduler_lab/lib/component/calendar.dart
+++ Project/scheduler_lab/lib/component/calendar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:scheduler_lab/const/colors.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatefulWidget {
@@ -14,6 +15,14 @@ class _CalendarState extends State<Calendar> {
   DateTime? selectedDay;
   DateTime? focusedDay;
 
+  final defaultBoxDeco = BoxDecoration(
+    color: Colors.grey[200],
+    borderRadius: BorderRadius.circular(6.0),
+  );
+
+  final defaultTextStyle =
+      TextStyle(color: Colors.grey[600], fontWeight: FontWeight.w700);
+
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
@@ -24,6 +33,24 @@ class _CalendarState extends State<Calendar> {
         formatButtonVisible: false, // 주, 2주, 월로 보기
         titleCentered: true,
         titleTextStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 32.0),
+      ),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: false, // 오늘 날짜 highlight
+
+        // 각각의 날짜의 container가 있는 데 그 container의 값을 지정해줌.
+        defaultDecoration: defaultBoxDeco,
+        weekendDecoration: defaultBoxDeco,
+        selectedDecoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(6.0),
+          border: Border.all(color: PRIMARY_COLOR, width: 1.0),
+        ),
+        // 평일, 주말 글자 스타일
+        defaultTextStyle: defaultTextStyle,
+        weekendTextStyle: defaultTextStyle,
+        selectedTextStyle: defaultTextStyle.copyWith(
+          color: PRIMARY_COLOR,
+        ),
       ),
       onDaySelected: ((selectedDay, focusedDay) {
         setState(() {
