7d43846a20c96a4ccf36a899bcb01b1fbfac167a
rookedsysc <rookedsysc36@gmail.com>
2022-10-13 03:55:14 +0900

    feat: Animated Opacity 구현

--- Theory/learn_animated_opacity/lib/screen/home_screen.dart
+++ Theory/learn_animated_opacity/lib/screen/home_screen.dart
@@ -13,15 +13,23 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 200.0,
-          height: 200.0,
-          color: Colors.green,
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 500),
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+          ),
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: (){
+          setState(() {
+            _visible = !_visible;
+          });
+
         },
         tooltip: 'Toggle Opacity',
         child: const Icon(Icons.flip),
