e7439c30f69b14a98513ce774f1054de36ec1b31
rookedsysc <rookedsysc36@gmail.com>
2022-10-13 22:19:05 +0900

    Feat : Project Create

---
+++ Project/dust_today/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:dust_today/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      themeMode: ThemeMode.dark,
+      home: HomeScreen(),
+    ),
+  );
+}
