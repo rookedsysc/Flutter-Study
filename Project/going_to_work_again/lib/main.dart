1426e02c9cd1c2231afe244f21628acab073dfd4
rookedsysc <rookedsysc36@gmail.com>
2022-11-08 13:23:29 +0900

    Feat : Project 생성 및 Google Maps 화면에 띄움

--- Project/going_to_work_again/lib/main.dart
+++ Project/going_to_work_again/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:going_to_work_again/screen/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       home: HomeScreen(),
