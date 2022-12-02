cbe8cccf7da430976b08909ace2407d02159ec94
rookedsysc <rookedsysc36@gmail.com>
2022-12-02 14:42:48 +0900

    feat : bottom Navigation Bar 추가

--- Project/lv2_actual/lib/common/layout/default_layout.dart
+++ Project/lv2_actual/lib/common/layout/default_layout.dart
@@ -5,12 +5,33 @@ import 'package:flutter/src/widgets/framework.dart';
 class DefaultLayout extends StatelessWidget {
   final Widget child;
   final Color? backgroundColor;
-  const DefaultLayout({required this.child, this.backgroundColor, super.key});
+  final String? title;
+  final Widget? bottomNavigationBar;
+  const DefaultLayout({required this.child, this.title, this.backgroundColor, this.bottomNavigationBar, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backgroundColor ?? Colors.white,
-        body: child);
+        appBar: renderAppBar(),
+        backgroundColor: backgroundColor ?? Colors.white,
+        body: child,
+      bottomNavigationBar: bottomNavigationBar
+    );
+  }
+
+  AppBar? renderAppBar() {
+    if(title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0, // Appbar가 앞으로 튀어나온거 같은 효과
+        title: Text(
+          title!,
+          style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
--- Project/lv2_actual/lib/common/view/root_tab.dart
+++ Project/lv2_actual/lib/common/view/root_tab.dart
@@ -2,14 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:lv2_actual/common/const/colors.dart';
 import 'package:lv2_actual/common/layout/default_layout.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({Key? key}) : super(key: key);
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      title: '코팩 딜리버리',
       child: Center(
-        child: Text('Root Tabk'),
+        child: Text('Root Tab'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
+        type: BottomNavigationBarType.shifting, // 선택된 탭이 조금 더 크게 보이게 만듬
+
+        // 선택된 탭의 인덱스가 들어가게 됨
+        onTap: (int index) {
+          debugPrint(index.toString());
+          setState(() {
+            this.index = index;
+          });
+        },
+        currentIndex: index,
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood_outlined),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outlined),
+            label: '프로필',
+          ),
+        ],
       ),
     );
   }
