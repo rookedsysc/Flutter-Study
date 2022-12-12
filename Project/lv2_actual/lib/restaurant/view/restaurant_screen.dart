36d3963f27ce3c104ce72c80b4012390c2011bd8
rookedsysc <rookedsysc36@gmail.com>
2022-12-12 12:45:13 +0900

    feat : restaurantStateNotifierProvider를 이용해서 데이터 받기

--- Project/lv2_actual/lib/restaurant/view/restaurant_screen.dart
+++ Project/lv2_actual/lib/restaurant/view/restaurant_screen.dart
@@ -9,6 +9,7 @@ import 'package:lv2_actual/common/model/cursor_pagination_model.dart';
 import 'package:lv2_actual/restaurant/component/restaurant_card.dart';
 import 'package:lv2_actual/restaurant/model/restaurant_model.dart';
 import 'package:lv2_actual/restaurant/repository/restaurant_repository.dart';
+import 'package:lv2_actual/restaurant/riverpod/restaurant_riverpod.dart';
 import 'package:lv2_actual/restaurant/view/restaurant_detail_screen.dart';
 
 class RestaurantScreen extends ConsumerWidget {
@@ -23,51 +24,46 @@ class RestaurantScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Padding(
+    // autoDispose를 사용하지 않았기 때문에 해당하는 Provider가 한 번 호출이 계속 생성이 된 상태로 있는다.
+    final data = ref.watch(restaurantStateNotifierProvider);
+
+    if (data.isNotEmpty) {
+      return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: FutureBuilder<CursorPagination<RestaurantModel>>(
-          future: ref.watch(restaurantRepositoryProvider).paginate(),
-          builder: (context, snapshot) {
+        child: ListView.separated(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final pItem = data[index];
 
-            if (!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            return ListView.separated(
-              itemCount: snapshot.data!.data.length,
-              itemBuilder: (context, index) {
-
-                final pItem = snapshot.data!.data[index];
-
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => RestaurantDetailScreen(id: pItem.id,),
-                    ));
-                  },
-                    child: RestaurantCard.fromModel(
-                        model: pItem));
-                // return RestaurantCard(
-                //   image: Image.network(
-                //     snapshot.data!.data.thumbUrl,
-                //     fit: BoxFit.cover,
-                //   ),
-                //   name: snapshot.data!.data.name,
-                //   tags: snapshot.data!.data.tags,
-                //   ratingsCount: snapshot.data!.data.ratingsCount,
-                //   deliveryTime: snapshot.data!.data.deliveryTime,
-                //   deliveryFree: snapshot.data!.data.deliveryFee,
-                //   ratings: snapshot.data!.data.ratings,
-                // );
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 16.0);
-              },
-            );
+            return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) => RestaurantDetailScreen(
+                      id: pItem.id,
+                    ),
+                  ));
+                },
+                child: RestaurantCard.fromModel(model: pItem));
+            // return RestaurantCard(
+            //   image: Image.network(
+            //     snapshot.data!.data.thumbUrl,
+            //     fit: BoxFit.cover,
+            //   ),
+            //   name: snapshot.data!.data.name,
+            //   tags: snapshot.data!.data.tags,
+            //   ratingsCount: snapshot.data!.data.ratingsCount,
+            //   deliveryTime: snapshot.data!.data.deliveryTime,
+            //   deliveryFree: snapshot.data!.data.deliveryFee,
+            //   ratings: snapshot.data!.data.ratings,
+            // );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 16.0);
           },
         ),
-      ),
-    );
+      );
+    } else {
+      return const Center(child: CircularProgressIndicator());
+    }
   }
 }
