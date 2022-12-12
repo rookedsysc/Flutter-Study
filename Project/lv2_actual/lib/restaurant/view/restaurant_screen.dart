16db84cd002e0c29577f3ff209c5e71c2aed3cee
rookedsysc <rookedsysc36@gmail.com>
2022-12-12 21:31:55 +0900

    feat : 데이터 가져올 때 실행되는 Loading Indicator 구현

--- Project/lv2_actual/lib/restaurant/view/restaurant_screen.dart
+++ Project/lv2_actual/lib/restaurant/view/restaurant_screen.dart
@@ -12,34 +12,76 @@ import 'package:lv2_actual/restaurant/repository/restaurant_repository.dart';
 import 'package:lv2_actual/restaurant/riverpod/restaurant_provider.dart';
 import 'package:lv2_actual/restaurant/view/restaurant_detail_screen.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget{
   const RestaurantScreen({super.key});
 
+  @override
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
   // json 데이터에서 data 키 안의 Resstaurant 배열을 가져오는 함수
-  Future<List<RestaurantModel>> paginateRestaurant({required WidgetRef ref}) async {
+  Future<List<RestaurantModel>> paginateRestaurant(
+      {required WidgetRef ref}) async {
     final dio = ref.watch(dioProvider);
-    final CursorPagination<RestaurantModel> resp = await RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant').paginate();
+    final CursorPagination<RestaurantModel> resp =
+        await RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant')
+            .paginate();
     return resp.data;
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    controller.addListener(scrollListener);
+
+    super.initState();
+  }
+
+  void scrollListener() {
+    // 현재 위치가 최대 길이보다 조금 덜 되는 위치까지 왔다면 새로운 데이터를 추가 요청
+    if (controller.offset >=
+        controller.position.maxScrollExtent - 300) {
+      // ref.read를 사용하면 해당하는 Provider가 한 번 호출이 계속 생성이 된 상태로 있음
+      ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // autoDispose를 사용하지 않았기 때문에 해당하는 Provider가 한 번 호출이 계속 생성이 된 상태로 있는다.
     final data = ref.watch(restaurantProvider);
 
     if (data is CursorPaginationLoading) {
       return const Center(child: CircularProgressIndicator());
-    } else {
+    }
 
-      // 현재 데이터의 타입은 CursorPaginationBase임
-      // 여기에서는 Data를 정상적으로 받아왔을 경우이므로 Data를 정상적으로 받아온 경우 Typedms CursorPagination임
-      // 따라서 data를 CursorPagination으로 형변환을 해줌 
-      final cp = data as CursorPagination;
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    // CursorPaggination || CursorPagginationFetchingMore || CursorPagginationRefetching 
+    // 현재 데이터의 타입은 CursorPaginationBase임
+    // 여기에서는 Data를 정상적으로 받아왔을 경우이므로 Data를 정상적으로 받아온 경우 Typedms CursorPagination임
+    // 따라서 data를 CursorPagination으로 형변환을 해줌
+    final cp = data as CursorPagination;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Scrollbar(
+        controller: controller,
         child: ListView.separated(
-          itemCount: cp.data.length,
-          itemBuilder: (context, index) {
+          controller: controller,
+          itemCount: cp.data.length + 1, // +1은 로딩을 위해서 
+          itemBuilder: (context, index) { 
+            if(index == cp.data.length) {
+              // 데이터가 FetchingMore(로딩 중)인 경우에만 ProgressIndicator를 보여줌
+              return Center(child: data is CursorPaginationFetchingMore ? const CircularProgressIndicator() : const SizedBox());
+            }
+
             final pItem = cp.data[index];
 
             return GestureDetector(
@@ -68,7 +110,7 @@ class RestaurantScreen extends ConsumerWidget {
             return const SizedBox(height: 16.0);
           },
         ),
-      );
-    }
+      ),
+    );
   }
 }
