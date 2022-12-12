db74a8d92e5460c96637871ace0913993079719f
rookedsysc <rookedsysc36@gmail.com>
2022-12-12 10:41:09 +0900

    feat : riverpod observer example

--- Theory/riverpod_sample/lib/main.dart
+++ Theory/riverpod_sample/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_sample/riverpod/provider_observer.dart';
 import 'package:riverpod_sample/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const ProviderScope( // Provider 쓸라면 Provider Scope가 상위에 존재해야 함
-    child: MaterialApp(
+    ProviderScope( // Provider 쓸라면 Provider Scope가 상위에 존재해야 함
+    observers: [
+      // Provider Scope 하위에 있는 모든 Provider들이 호출될 때 실행함
+      Logger(),
+    ],
+    child: const MaterialApp(
       home: HomeScreen(),
     ),
   ));
