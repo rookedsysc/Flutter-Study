6086404599e2233e8c6c8f48ca65223a08faf010
rookedsysc <rookedsysc36@gmail.com>
2022-12-24 02:56:44 +0900

    feat : User Me Repository, Provider 생성

--- Project/lv2_actual/lib/user/model/user_model.dart
+++ Project/lv2_actual/lib/user/model/user_model.dart
@@ -3,8 +3,19 @@ import 'package:lv2_actual/common/utils/data_utils.dart';
 
 part 'user_model.g.dart';
 
+// 현재 로그인의 상태 값을 담기 위한 클래스
+// CursorPaginationBase와 같은 개념임
+abstract class UserModelBase {}
+
+class UserModelError extends UserModelBase {
+  final String message;
+  UserModelError({required this.message});
+}
+
+class UserModelLoading extends UserModelBase {}
+
 @JsonSerializable()
-class UserModel {
+class UserModel extends UserModelBase{
   final String id;
   final String username;
   @JsonKey(fromJson: DataUtils.pathToUrl)
---
+++ Project/lv2_actual/lib/user/provider/user_me_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:lv2_actual/common/const/data.dart';
+import 'package:lv2_actual/user/model/user_model.dart';
+import 'package:lv2_actual/user/repository/user_me_repository.dart';
+
+class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final UserMeRepository repository;
+  final FlutterSecureStorage storage;
+  UserMeStateNotifier({required this.storage, required this.repository}): super(
+    UserModelLoading()
+  ) {
+    getMe();
+  }
+
+  Future<void> getMe() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+    
+    // 최소한 RefreshToken과 AccessToken이 있어야만 getMe 요청을 보낼 수 있음
+    if(refreshToken == null || accessToken == null) {
+      return; 
+    }
+
+    final resp = await repository.getMe();
+    state = resp; // 상태에 UserModel 저장 
+  }
+}
