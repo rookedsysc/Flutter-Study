a2dcc6e5a8535012b7464d99bec4338bd1f5cbb2
rookedsysc <rookedsysc36@gmail.com>
2022-12-28 17:09:14 +0900

    feat : Order Model 생성(#24)

---
+++ Project/lv2_actual/lib/order/model/order_model.dart
@@ -0,0 +1,69 @@
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:lv2_actual/common/model/model_with_id.dart';
+import 'package:lv2_actual/common/utils/data_utils.dart';
+import 'package:lv2_actual/restaurant/model/restaurant_model.dart';
+
+part 'order_model.g.dart';
+
+@JsonSerializable()
+class OrderProductModel {
+  final String id;
+  final String name;
+  final String detail;
+  @JsonKey(
+    fromJson: DataUtils.pathToUrl
+  )
+  final String imgUrl;
+  final int price; 
+
+  OrderProductModel({
+    required this.detail,
+    required this.id,
+    required this.imgUrl,
+    required this.name,
+    required this.price
+  });
+
+  factory OrderProductModel.fromJson(Map<String, dynamic> json) => _$OrderProductModelFromJson(json);
+  Map<String, dynamic> toJson() => _$OrderProductModelToJson(this);
+}
+
+@JsonSerializable()
+class OrderProductAndCountModel {
+  final OrderProductModel product;
+  final int count;
+
+  OrderProductAndCountModel({
+    required this.product,
+    required this.count,
+  });
+
+  factory OrderProductAndCountModel.fromJson(Map<String, dynamic> json) => _$OrderProductAndCountModelFromJson(json);
+  Map<String, dynamic> toJson() => _$OrderProductAndCountModelToJson(this);
+}
+
+@JsonSerializable()
+class OrderModel implements IModelWithId {
+  final String id;
+  final List<OrderProductAndCountModel> products;
+  final int totalPrice;
+  final RestaurantModel restaurant;
+  @JsonKey(
+    fromJson: DataUtils.stringToDateTime
+  )
+  final DateTime createAt;
+
+  OrderModel({
+    required this.id,
+    required this.products,
+    required this.totalPrice,
+    required this.restaurant,
+    required this.createAt
+  });
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) => _$OrderModelFromJson(json);
+  Map<String, dynamic> toJson() => _$OrderModelToJson(this);
+}
